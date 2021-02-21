a262ba777f9caba26371ad37c6ac03f52d8a453b
Test User <test@mail.ru>
2021-02-21 12:40:32 +0300

    Initial commit

---
+++ src/ru/jegensomme/Main.java
@@ -0,0 +1,10 @@
+package ru.jegensomme.test
+
+public static class Main {
+    
+    public static main(String[] args) {
+        for (String arg : args) {
+            System.out.println("arg:" + arg);
+        }
+    }
+}
