a78ce4a31273d383ec13191c003c145c5da10800
Test User <test@mail.ru>
2021-02-21 13:10:24 +0300

    fix

---
+++ src/ru/jegensomme/test/Main.java
@@ -0,0 +1,10 @@
+package ru.jegensomme.test;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("arg:" + arg);
+        }
+    }
+}
